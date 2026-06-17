2310ae5700f3244c0432b0a588aeb2441456dea1
arjnks <arjun.s2024c@vitstudent.ac.in>
2026-06-17 12:08:28 +0530

    Refine mobile header to un-stick and shrink nav elements

---
+++ model-2/bump.py
@@ -0,0 +1,8 @@
+﻿import os, re
+for f in os.listdir('.'):
+    if f.endswith('.html'):
+        with open(f, 'r', encoding='utf-8') as file:
+            content = file.read()
+        content = re.sub(r'href=\"css/style\.css(\?v=\d+)?\"', 'href=\"css/style.css?v=6\"', content)
+        with open(f, 'w', encoding='utf-8') as file:
+            file.write(content)
